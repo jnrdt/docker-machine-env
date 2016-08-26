d11dac46da4848fec4dbdf4a1c9cc260d47d9ed0
jnrdt <github@jnrdt.com>
2016-08-26 23:00:17 +0200

    base function

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const _ = require('lodash');
+const child_process = require('child_process');
+
+module.exports = function(host, callback){
+
+	child_process.exec(`docker-machine env ${host}`, function(envError, envStdout, envStderr){
+		
+		var envs;
+		
+		if(!envError && !envStderr)
+		{
+			envs = _.chain(envStdout.split('\n'))
+			.filter((cmd) => {
+
+				return cmd.split(' ')[0] == "SET";
+			})
+			.map((cmd) => {
+				
+				var keyvalue = cmd.split(' ')[1].split('=');
+				return [keyvalue[0], keyvalue[1]];
+			})
+			.fromPairs()
+			.value();
+		}
+		
+		callback(envError || envStderr, envs);
+	})
+}
